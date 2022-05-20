f9b52fa26e67f71e68183a51e798bb637bfeb5b5
Harishwaran <harishwaranmail@gmail.com>
2022-05-20 14:33:45 +0530

    signup page created

--- front-end/myntra/src/App.js
+++ front-end/myntra/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Home from './components/Pages/Home/Home'
+import Home from './components/Pages/Home/Home';
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Home/>
-      <Footer/>
+      <Signup/>
+      {/* <Home/>
+      <Footer/> */}
     </div>
   );
 }
